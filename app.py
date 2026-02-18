5ea6ca2677ad6459664b134816851521418b41da
andresilaen60-pixel <andresilaen60@gmail.com>
2026-02-18 14:38:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -142,6 +142,12 @@ if not st.session_state.logged_in:
     # Ambil nilai menu dari session state untuk logika tampilan
     menu_pilihan = st.session_state.menu_aktif
 
+    # Tambahkan di bagian paling bawah 'with st.sidebar:'
+    st.write("---")
+    if st.button("🚪 Logout", use_container_width=True):
+        st.session_state.logged_in = False
+        st.rerun()
+        
     # --- 6. TAMPILAN BERDASARKAN MENU ---
     if menu_pilihan == "Data Provinsi":
         st.header("🏢 Rekapitulasi Guru Provinsi")
@@ -312,6 +318,7 @@ if not st.session_state.logged_in:
                     with st.expander(f"📖 {row['Jabatan']}"):
                         st.write(f"Guru: {int(row['Jml Guru'])} | ABK: {int(row['ABK'])}")
                         
+
 
 
 
